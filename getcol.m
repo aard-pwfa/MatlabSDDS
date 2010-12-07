63657a36d0bac2c6545a4188ffe80554389026c1
Joel Frederico <joelfred@slac.stanford.edu>
2010-12-07 13:09:15 -0800

    Added page support in plotcolumn, multicolumn support in getcol.

--- getcol.m
+++ getcol.m
@@ -1,4 +1,22 @@
-function out=getcol(filename,colname)
+function varargout=getcol(filename,colname)
 	sdds=sddsload(filename);
-	out=converttovector(sdds.column.(colname));
+	
+	if strcmp(sdds.description.text,'FirstPointRef')
+		first=2;
+	else
+		first=1;
+	end
+	switch class(colname)
+		case 'char'
+			out=converttovector(sdds.column.(colname));
+			out=out(first:end);
+		case 'cell'
+			colsize=size(colname,2)
+			out=cell(colsize,1);
+			for i=1:colsize
+				out{i}=converttovector(sdds.column.(colname{i}));
+				out{i}=out{i}(first:end);
+			end
+	end
+	varargout=out;
 end
--- plotcolumn.m
+++ plotcolumn.m
@@ -7,25 +7,33 @@ function varargout=plotcolumn(filename,colstr,formatstr)
 				formatstr={'-','-'};
 		end
 	end
+	if strcmp(class(filename),'cell')
+		page=filename{2};
+		filename=filename{1};
+	else
+		page=1;
+	end
+
 	sdds = sddsload(filename);
-	if sdds.description.text=='FirstPointRef'
+	if strcmp(sdds.description.text,'FirstPointRef')
 		first=2;
 	else
 		first=1;
 	end
+
 	switch class(colstr)
 		case 'char'
-			y=converttovector(sdds.column.(colstr));
+			y=converttovector(sdds.column.(colstr),page);
 			y=y(first:end);
 			plot(y,formatstr);
 			addlabels('Array Index',colstr,'');
 			out={y};
 		case 'cell'
-			x=converttovector(sdds.column.(colstr{1}));
-			y=converttovector(sdds.column.(colstr{2}));
+			x=converttovector(sdds.column.(colstr{1}),page);
+			y=converttovector(sdds.column.(colstr{2}),page);
 			x=x(first:end);
 			y=y(first:end);
-			plot(x,y);
+			plot(x,y,formatstr);
 			addlabels(colstr{1},colstr{2},'');
 			out={x,y};
 	end
