b6d912b5acf8fe7798289651086f883407096030
Joel Frederico <joelfred@slac.stanford.edu>
2010-12-07 11:35:30 -0800

    Added support for handling runs that have a fiducial run.
    Now plotcolumn won't plot the first data point if the file is marked
    that the first data point is a fiducial run, so that only the iterative
    data points are shown.

--- plotcolumn.m
+++ plotcolumn.m
@@ -8,15 +8,23 @@ function varargout=plotcolumn(filename,colstr,formatstr)
 		end
 	end
 	sdds = sddsload(filename);
+	if sdds.description.text=='FirstPointRef'
+		first=2;
+	else
+		first=1;
+	end
 	switch class(colstr)
 		case 'char'
 			y=converttovector(sdds.column.(colstr));
+			y=y(first:end);
 			plot(y,formatstr);
 			addlabels('Array Index',colstr,'');
 			out={y};
 		case 'cell'
 			x=converttovector(sdds.column.(colstr{1}));
 			y=converttovector(sdds.column.(colstr{2}));
+			x=x(first:end);
+			y=y(first:end);
 			plot(x,y);
 			addlabels(colstr{1},colstr{2},'');
 			out={x,y};
