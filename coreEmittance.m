5ea5a1ef3af2811fabc0ed128b3d63d77c4a6c17
Joel Frederico <joelfred@slac.stanford.edu>
2011-05-06 11:58:54 -0700

    Added coreEmittance.m.

---
+++ coreEmittance.m
@@ -0,0 +1,169 @@
+function varargout=coreEmittance(filename,fraction)
+	display('This calculation expects a corresponding .twi file with the last Twiss entry corresponding to the particle dump!');
+	sdds=sddsload(filename);
+	unix(['sddsanalyzebeam ' filename ' ' filename '.ana']);
+
+	[x,xp,y,yp,t,p,pID]=extractphasespace(filename);
+	partlist=[pID, x, xp, y, yp, t, p];
+	% size(partlist)
+	numParts=size(pID,1)
+	targetParts=round(numParts*fraction)
+	
+
+	% Loads emittance from analyzed particle file
+	ex     = getcol('momentumscan.out.ana','ex');
+	ey     = getcol('momentumscan.out.ana','ey');
+	% =========================================================
+	% =========================================================
+	% 		Pick a twiss loading method here.
+	% =========================================================
+	% =========================================================
+	% ---------------------------------------------------------
+	% Loads twiss parameters from .twi file.
+	% ---------------------------------------------------------
+	betax  = getcol('momentumscan.twi','betax');
+	betay  = getcol('momentumscan.twi','betay');
+	alphax = getcol('momentumscan.twi','alphax');
+	alphay = getcol('momentumscan.twi','alphay');
+	betax  = betax(end);
+	betay  = betay(end);
+	alphax  = alphax(end);
+	alphay  = alphay(end);
+	% ---------------------------------------------------------
+	% Loads twiss pparameters from analyzed particle file.
+	% ---------------------------------------------------------
+	% betax  = getcol('momentumscan.out.ana','betax');
+	% betay  = getcol('momentumscan.out.ana','betay');
+	% alphax = getcol('momentumscan.out.ana','alphax');
+	% alphay = getcol('momentumscan.out.ana','alphay');
+	% =========================================================
+	% =========================================================
+	% Calculates gamma from alpha, beta.
+	gammax = (1+alphax^2)/betax;
+	gammay = (1+alphay^2)/betay;
+	twiss = struct('ex',ex,'ey',ey,'betax',betax,'betay',betay,'alphax',alphax,'alphay',alphay,'gammax',gammax,'gammay',gammay);
+
+	csparamx = [betax, alphax, gammax,ex];
+	csparamy = [betay, alphay, gammay,ey];
+	
+	% Since each particle is summed over, the contribution
+	% of each particle to the 4-D emittance is calc. first.
+	% The requested fraction with the smallest contributions
+        % are kept.
+	begfrac=0;
+	endfrac=10;
+	midfrac=mean([begfrac,endfrac]);
+	midpt=-1;
+	xarea=gammax*x.^2+2*alphax.*x.*xp+betax.*xp.^2;
+	yarea=gammay*y.^2+2*alphay.*y.*yp+betay.*yp.^2;
+	emit4d=xarea/ex+yarea/ey;
+
+	partlist = [partlist, xarea, yarea, emit4d];
+	
+	psorted=sortrows(partlist,10);
+	
+	pcut=psorted(1:round(numParts*fraction),:);
+	ex=emit(pcut(:,2),pcut(:,3))
+
+	todump=psorted(targetParts+1:end,:);
+	dumpparts(todump,sdds,'rejected.out');
+	dumpparts(pcut,sdds,'saved.out');
+	varargout={psorted(round(numParts*fraction),:),twiss};
+	unix('sddsanalyzebeam saved.out saved.out.ana');
+	unix(['sddsprintout ' filename '.ana -col=ecn*'])
+	unix('sddsprintout saved.out.ana -col=ecn*')
+
+	% while midpt~=targetParts
+	%         begpt=numInEllipse(partlist,begfrac,ex,ey);
+	%         midpt=numInEllipse(partlist,midfrac,ex,ey);
+	%         endpt=numInEllipse(partlist,endfrac,ex,ey);
+	%         if begpt>targetParts
+	%                 begfrac=begfrac/10;
+	%         elseif endpt<targetParts
+	%                 endfrac=endfrac*10;
+	%         elseif midpt>targetParts
+	%                 endfrac=midfrac;
+	%         elseif midpt<targetParts
+	%                 begfrac=midfrac;
+	%         end
+	%         midfrac=mean([begfrac,endfrac]);
+	% end
+	% varargout={midfrac,twiss};
+	% [tot,bool]=numInEllipse(partlist,midfrac,ex,ey);
+	% ex=emit(x.*bool,xp.*bool)
+
+	% dumptot=sum(~bool)
+	% npartlist = [pID.*(~bool),x,xp,y,yp,t,p];
+	% psorted   = sortrows(npartlist,-1);
+	% todump    = psorted(1:dumptot,:);
+	% dumpparts(todump,sdds,'rejected.out');
+	% dumpparts(psorted(dumptot+1:end,:),sdds,'saved.out');
+
+end
+
+function varargout=numInEllipse(partlist,frac,ex,ey)
+	bx=partlist(:,8)<frac*ex;
+	by=partlist(:,9)<frac*ey;
+	% sum(by)
+	bool=bx&by;
+	tot=sum(bool);
+
+	if nargout==2
+		varargout={tot,bool};
+	else
+		varargout={tot};
+	end
+end
+
+function dumpparts(todump,sdds,filename)
+	sdds.column.x.page1          = todump(:,2);
+	sdds.column.xp.page1         = todump(:,3);
+	sdds.column.y.page1          = todump(:,4);
+	sdds.column.yp.page1         = todump(:,5);
+	sdds.column.t.page1          = todump(:,6);
+	sdds.column.p.page1          = todump(:,7);
+	sdds.column.particleID.page1 = todump(:,1);
+	sdds.ascii=1;
+	sddssave(sdds,filename);
+
+end
+
+function varargout=sigfrac(partlist,frac)
+
+	x  = partlist(:,2);
+	xp = partlist(:,3);
+	y  = partlist(:,4);
+	yp = partlist(:,5);
+	t  = partlist(:,6);
+	p  = partlist(:,7);
+
+	xc  = x  - mean(x);
+	xpc = xp - mean(xp);
+	yc  = y  - mean(y);
+	ypc = yp - mean(yp);
+	tc  = t  - mean(t);
+	pc  = p  - mean(p);
+
+	sigx  = std(xc);
+	sigxp = std(xpc);
+	sigy  = std(yc);
+	sigyp = std(ypc);
+	sigt  = std(tc);
+	sigp  = std(pc);
+
+	bx  = abs(xc)  < frac*sigx;
+	bxp = abs(xpc) < frac*sigxp;
+	by  = abs(yc)  < frac*sigy;
+	byp = abs(ypc) < frac*sigyp;
+	bt  = abs(tc)  < frac*sigt;
+	bp  = abs(pc)  < frac*sigp;
+	
+	bool=bx|bxp|by|byp|bt|bp;
+	tot=sum(bool);
+
+	if nargout==2
+		varargout={tot,bool}
+	else
+		varargout={tot};
+	end
+end
