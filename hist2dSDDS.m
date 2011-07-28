4852b537c5195782cbfb0944fe2b6c0365de6ba2
Joel Frederico <joelfred@slac.stanford.edu>
2011-07-28 11:14:20 -0700

    Create 2d hist from an SDDS file.

--- hist2dSDDS.m
+++ hist2dSDDS.m
@@ -4,8 +4,15 @@ function h=hist2dSDDS(filename,xs,ys,res,varargin)
 	else
 		pagenum=varargin{1};
 	end
+	switch nargin
+		case 5
+			nstd=20;
+		case 6
+			nstd=varargin{2};
+	end
 	if exist(filename,'dir')==7
-		filename=[filename '/' sprintf('%3.3i',pagenum) '.out']
+		filename=[filename '/' sprintf('%3.3i',pagenum) '.out'];
+		pagenum=1;
 	end
 	[x,xp,y,yp,t,p]=extractphasespace(filename,pagenum);
 	c=299792458;
@@ -13,7 +20,7 @@ function h=hist2dSDDS(filename,xs,ys,res,varargin)
 	dp=p/mean(p)-1;
 	xs=eval(xs);
 	ys=eval(ys);
-	bool=(xs-mean(xs) > 20*std(xs));
+	bool=(xs-mean(xs) > nstd*std(xs));
 	xs(bool)=[];
 	ys(bool)=[];
 	hist2d(xs,ys,res);
